dd43c7f5614d6e598330baf1db4b74eb877b2eda
Rodrigo Rivas <rodroy019@hotmail.com>
2024-03-26 01:06:36 -0300

    add: se agregan aspectos y estilos, falta el search

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,17 @@
-const Footer = () => {
+import Container from 'react-bootstrap/Container'
 
+function Footer () {
+  return (
+    <footer className='bg-dark text-light'>
+      <Container fluid>
+        <p className='text-center py-3 m-0'>
+          Este es una pagina para propósitos educativos de Desafio LATAM. Todos los derechos reservados.
+          <br />
+          Desarrollado por Rodrigo Rivas Figueroa.
+        </p>
+      </Container>
+    </footer>
+  )
 }
 
 export default Footer
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,23 +6,34 @@ import Form from 'react-bootstrap/Form'
 function Header () {
   return (
     <Navbar bg='dark' data-bs-theme='dark' expand='lg' className='bg-body-tertiary fixed-top'>
-      <Container>
-        <Navbar.Brand href='#home'>Marvel</Navbar.Brand>
-        <Nav className='me-auto'>
-          <Nav.Link href='#personajes'>Personajes</Nav.Link>
-          <Nav.Link href='#comics'>Comics</Nav.Link>
-          <Nav.Link href='#series'>Series</Nav.Link>
-          <Nav.Link href='#eventos'>Eventos</Nav.Link>
-        </Nav>
+      <Container fluid>
+        <Navbar.Brand href='#home' className='me-0 me-lg-4'>
+          <img
+            src='src/assets/img/logo.png'
+            alt='Marvel Logo'
+            height='30'
+            className='d-inline-block align-top me-2'
+            style={{ marginLeft: '10px' }}
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls='navbar-nav' />
+        <Navbar.Collapse id='navbar-nav' className='justify-content-between'>
+          <Nav className='me-auto'>
+            <Nav.Link href='#personajes'>Personajes</Nav.Link>
+            <Nav.Link href='#comics'>Comics</Nav.Link>
+            <Nav.Link href='#series'>Series</Nav.Link>
+            <Nav.Link href='#eventos'>Eventos</Nav.Link>
+          </Nav>
+          <Form className='d-flex'>
+            <Form.Control
+              type='search'
+              placeholder='Buscador'
+              className='me-2'
+              aria-label='Search'
+            />
+          </Form>
+        </Navbar.Collapse>
       </Container>
-      <Form className='d-flex'>
-        <Form.Control
-          type='search'
-          placeholder='Search'
-          className='me-2'
-          aria-label='Search'
-        />
-      </Form>
     </Navbar>
   )
 }
